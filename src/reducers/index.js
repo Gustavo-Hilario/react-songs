bd6ddc13ad755a1b1a881387c09904b59215445c
Gustavo_H <gustavo.hilario@automattic.com>
2022-09-17 09:49:13 -0300

    235. Building Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        { title: "No Scrubs", duration: "4:05" },
+        { title: "Macaren", duration: "2:44" },
+        { title: "All Start", duration: "3:35" },
+        { title: "I want it that way", duration: "4:55" },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED") {
+        return action.payload;
+    }
+    return selectedSong;
+};
